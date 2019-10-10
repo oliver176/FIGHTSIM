41c76cafde87b1a3638234faa6255527d7970095
oliver176 <55237259+oliver176@users.noreply.github.com>
2019-10-10 13:29:08 +0200

    Intial V2 Commit

---
+++ FightSimV2/FightSimV2/Fighter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightSimV2
+{
+    class Fighter
+    {
+        int hp;
+        int dmg;
+        public string name;
+        Random gen = new Random();
+
+        public Fighter(string getName)   //input för namn
+        {
+            hp = 100;
+            dmg = gen.Next(3, 20);
+            name = getName;
+        }
+
+        public int Attack()
+        {
+            return dmg;
+        }
+        public void Hurt(int amount)
+        {
+            hp -= amount;   //ta dmg amount
+        }
+        public bool IsAlive()
+        {
+            if (hp > 0) //om den lever return true
+            {
+                return true;
+            }
+            else return false;
+        }
+        public int GetHp()
+        {
+            return hp;
+        }
+
+    }
+}
---
+++ FightSimV2/FightSimV2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightSimV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name the 2 fighters!");
+
+            Fighter fighterA = new Fighter(Console.ReadLine()); //skapar 2 fighter med namn input
+            Fighter fighterB = new Fighter(Console.ReadLine());
+
+            while (true)
+            {
+                if (fighterA.IsAlive() && fighterB.IsAlive())  //när båda lever
+                {
+                    Console.WriteLine(fighterA.name + fighterA.GetHp());  //Skriv ut deras hp
+                    Console.WriteLine(fighterB.name + fighterB.GetHp());
+
+                    fighterB.Hurt(fighterA.Attack());
+                    fighterA.Hurt(fighterB.Attack());  //ta skada från den andras attack
+
+                    Console.WriteLine("___________");
+                    Console.ReadLine();
+                }
+                else if (fighterA.IsAlive() == false || fighterB.IsAlive() == false) //Om någon är död
+                {
+                    if (fighterA.IsAlive())
+                    {
+                        Console.WriteLine("Player A WINS!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Player B WINS");
+                    }
+                    break; // :)
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
