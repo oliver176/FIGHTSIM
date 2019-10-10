851e999c540f747648319f912356780e003d2fb4
oliver176 <55237259+oliver176@users.noreply.github.com>
2019-10-10 12:48:43 +0200

    Initial commit

---
+++ FightSimV1/FightSimV1/Fighter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightSimV1
+{
+    class Fighter
+    {
+        int hp;
+        public string name;
+
+        public int Attack()
+        {
+
+        }
+        public void Hurt(int amount)
+        {
+
+        }
+        public bool IsAlive()
+        {
+
+        }
+        public int GetHp()
+        {
+
+        }
+    }
+}
